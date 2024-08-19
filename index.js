c076ab9141ac4a63b65b882adb1d3a85c572c5e9
areeba-imran2 <areebaimran.edu@gmail.com>
2024-08-19 19:32:06 +0500

    Updated index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import fs from 'fs';  // Correct import for fs module
+import { createServer } from 'http';
+
+const myServer = createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} New Req Received\n`;
+
+  fs.appendFile('log.txt', log, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      res.end('Error occurred');
+      return;
+    }
+    res.end('Hello From Server Again');
+  });
+});
+
+myServer.listen(8000, () => console.log('Server Started!'));
